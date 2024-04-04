9174d9a6cc2221d311bd793fb14b97bd878fec93
Mateusz Tacik <seqi@interia.pl>
2024-04-04 20:50:54 +0200

    v0.2

--- index.php
+++ index.php
@@ -6,9 +6,39 @@
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="css/style.css">
     <script src="js/script.js" defer></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
     <title>Cinema - Stellar Screens</title>
 </head>
 <body>
-    
+    <div class="navigation-bar">
+        <h3>
+            Stellar_Screens
+        </h3>
+        <div class="links">
+            <div class="link">
+                information
+            </div>
+            <div class="link">
+                movies
+            </div>
+            <div class="link">
+                login
+            </div>
+        </div>
+    </div>
+    <header>
+        <h1>
+            The Best Cinema in Galaxy
+        </h1>
+        <h2>
+            Come to us to experience the best movies on the best screens.
+            <br>
+            Our cinema has the newest screens and stellar experience to offer even for 20% off!
+            <br>
+            We are located on ...street and open from 5am to 12pm.
+        </h2>
+    </header>
 </body>
 </html>
