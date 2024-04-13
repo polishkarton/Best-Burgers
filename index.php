fadf78bdb430a558632345ec13a068cf67e72b83
Mateusz Tacik <seqi@interia.pl>
2024-04-13 20:39:31 +0200

    initial commit

--- index.php
+++ index.php
@@ -4,41 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="css/style.css">
-    <script src="js/script.js" defer></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
     <title>Cinema - Stellar Screens</title>
 </head>
 <body>
-    <div class="navigation-bar">
-        <h3>
-            Stellar_Screens
-        </h3>
-        <div class="links">
-            <div class="link">
-                information
-            </div>
-            <div class="link">
-                movies
-            </div>
-            <div class="link">
-                login
-            </div>
+    <nav>
+        <div class="logo">
+            Best Burgers
         </div>
-    </div>
+        <div class="menu">
+            <span class="material-icons">
+                menu
+            </span>
+        </div>
+    </nav>
     <header>
-        <h1>
-            The Best Cinema in Galaxy
-        </h1>
-        <h2>
-            Come to us to experience the best movies on the best screens.
-            <br>
-            Our cinema has the newest screens and stellar experience to offer even for 20% off!
-            <br>
-            We are located on ...street and open from 5am to 12pm.
-        </h2>
+        <div class="header-container">
+            <h1>The best Burgers in town!</h1>
+            <a href="#">
+                <p>Create burgers in your style</p>
+            </a>
+        </div>
     </header>
 </body>
 </html>
