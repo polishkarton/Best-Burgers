ebbb354c6908148267d3cf54eaee642ce49d5c3b
Mateusz Tacik <seqi@interia.pl>
2024-04-18 18:52:02 +0200

    newest color

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const menu = document.querySelector('span');
+const dropdown = document.querySelector('#nav-box');
+
+menu.addEventListener('click', () => {
+    if(menu.innerHTML == 'menu'){
+        menu.innerHTML = 'close';
+        dropdown.classList.add('active');
+    }else {
+        menu.innerHTML = 'menu';
+        dropdown.classList.remove('active');
+    }
+});
