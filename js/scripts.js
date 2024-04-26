1b6e3810b803cd68e24f99297e55610baa7270dc
Mateusz Tacik <seqi@interia.pl>
2024-04-26 17:04:27 +0200

    working menu

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,59 @@ const menu = document.querySelector('span');
 const dropdown = document.querySelector('#nav-box');
 
 menu.addEventListener('click', () => {
-    if(menu.innerHTML == 'menu'){
+    if (menu.innerHTML == 'menu') {
         menu.innerHTML = 'close';
         dropdown.classList.add('active');
-    }else {
+    } else {
         menu.innerHTML = 'menu';
         dropdown.classList.remove('active');
     }
 });
+
+const menuLinks = document.querySelectorAll('.link');
+
+menuLinks.forEach(elem => {
+    elem.addEventListener('click', () => {
+        menu.innerHTML = 'menu';
+        dropdown.classList.remove('active');
+    })
+})
+
+
+const burger = document.getElementById('burger-container');
+const options = document.querySelectorAll('.option');
+const bun = document.getElementsByClassName('bun');
+
+function plusFunc(ingredient, burger, bun) {
+    const newIngredient = document.createElement("div");
+    newIngredient.classList.add(ingredient);
+    newIngredient.innerHTML = ingredient.toUpperCase();
+    burger.insertBefore(newIngredient, bun);
+}
+
+function minusFunc(ingredient) {
+    const ing = document.getElementsByClassName(ingredient);
+    if (ing.length > 0) {
+        ing[0].remove();
+    } else {
+        console.log('error no more ' + ingredient);
+    }
+}
+
+options.forEach((elem) => {
+    const plus = elem.getElementsByClassName('add')[0];
+    const minus = elem.getElementsByClassName('delete')[0];
+    const elemClass = elem.title;
+    plus.addEventListener('click', () => {
+        plusFunc(elemClass, burger, bun[1]);
+    });
+
+    minus.addEventListener('click', () => {
+        minusFunc(elemClass);
+    });
+});
+
+
+
+
+
